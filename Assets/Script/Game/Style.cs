4b4a7f22b1671021dfa49e328659f2da7694df63
Wavepoole <49838248+Wavepoole@users.noreply.github.com>
2023-05-24 15:47:09 +1000

    Adrenaline added to style script

--- Assets/Script/Game/Style.cs
+++ Assets/Script/Game/Style.cs
@@ -10,6 +10,9 @@ public class Style : MonoBehaviour
     public string rankTitle;
     public float multikillTimer;
     public int multikillCount;
+    public float adrenalinePoints;
+    public float adrenalineGain;
+    public bool hasAdrenaline;
     void Start()
     {
         rankTitle = "";
@@ -17,6 +20,7 @@ public class Style : MonoBehaviour
         styleDecay = 0f;
         multikillTimer = 0f;
         multikillCount = 0;
+        hasAdrenaline = true;
     }
 
     // Update is called once per frame
@@ -24,6 +28,7 @@ public class Style : MonoBehaviour
     {
         //decay and multikill checks
         styleAmount -= styleDecay * Time.deltaTime;
+        adrenalinePoints += adrenalineGain;
         multikillTimer -= Time.deltaTime;
         if (multikillTimer <= 0)
         {
@@ -37,49 +42,69 @@ public class Style : MonoBehaviour
             styleAmount = 0;
             rankTitle = "";
             styleDecay = 0f;
+            adrenalineGain = 1;
         }
-        else if (styleAmount < 100)
+
+        else if (styleAmount > 0 && styleAmount < 100)
         {
             rankTitle = "Deputy";
             styleDecay = 5f;
+            adrenalineGain = 2;
         }
+
         else if (styleAmount >= 100 && styleAmount < 250)
         {
             rankTitle = "Sherrif";
             styleDecay = 7.5f;
+            adrenalineGain = 3;
         }
+
         else if (styleAmount >= 250 && styleAmount < 450)
         {
             rankTitle = "Vigilante";
             styleDecay = 12.5f;
+            adrenalineGain = 5;
         }
+
         else if (styleAmount >= 450 && styleAmount < 700)
         {
             rankTitle = "Hero";
             styleDecay = 20f;
+            adrenalineGain = 8;
         }
+
         else if (styleAmount >= 700 && styleAmount < 1000)
         {
             rankTitle = "Legend";
             styleDecay = 32.5f;
+            adrenalineGain = 13;
         }
+
         else if (styleAmount >= 1000)
         {
             rankTitle = "Myth";
             styleDecay = 50f;
+            adrenalineGain = 21;
+        }
+
+        //adrenaline stuff
+        if (adrenalinePoints >= 100)
+        {
+            adrenalinePoints = 100;
+            hasAdrenaline = true;
         }
     }
     public void enemyKill()
     {
-        styleAmount += 50f;
+        styleAmount += 75f;
         multikillCount++;
         multikillTimer = 3f;
         if (multikillCount == 2)
-            styleAmount += 15f;
+            styleAmount += 25f;
         else if (multikillCount == 3)
-            styleAmount += 30f;
-        else if (multikillCount >= 4)
             styleAmount += 50f;
+        else if (multikillCount >= 4)
+            styleAmount += 75f;
     }
     public void bulletBlock()
     {
@@ -88,5 +113,12 @@ public class Style : MonoBehaviour
     public void enemyStun()
     {
         styleAmount += 20f;
+    }
+    public void takeDamage()
+    {
+        hasAdrenaline = false;
+        adrenalinePoints = 0;
+        styleAmount -= 200f;
+       
     }
 }
